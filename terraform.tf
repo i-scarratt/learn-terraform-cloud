c84af4e7f8b30e0c1efede892dbbc4ed2e60d12f
i-scarratt <i-scarratt>
2024-02-28 11:25:29 +0000

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "i-scarratt-tf"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
+*/
 
   required_providers {
     aws = {
